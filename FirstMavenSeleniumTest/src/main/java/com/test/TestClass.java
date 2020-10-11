a39f5f3611b292e4a28facf2d72085d4014598bf
mouna <mouna.torki@gmail.com>
2020-10-10 17:00:55 -0900

    commit

--- FirstMavenSeleniumTest/src/main/java/com/test/TestClass.java
+++ FirstMavenSeleniumTest/src/main/java/com/test/TestClass.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -16,9 +17,12 @@ public class TestClass {
 		{
 		  System.out.println("debut test");
 				
-			DesiredCapabilities cap=new DesiredCapabilities();
-		    cap.setBrowserName("chrome");
-		    WebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+//			DesiredCapabilities cap=new DesiredCapabilities();
+//		    cap.setBrowserName("chrome");
+//		    WebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+		    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
 		    driver.get("https://www.facebook.com/");
 			driver.findElement(By.xpath("//input[@id='email']")).sendKeys("0652455762");
 			driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("jamali11*");
