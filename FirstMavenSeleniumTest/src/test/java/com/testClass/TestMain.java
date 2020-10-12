e100e7c3f9b0cbe9ed4ad9e3f63a905493c56ed3
mouna <mouna.torki@gmail.com>
2020-10-12 00:21:50 -0900

    commit hub

--- FirstMavenSeleniumTest/src/test/java/com/testClass/TestMain.java
+++ FirstMavenSeleniumTest/src/test/java/com/testClass/TestMain.java
@@ -17,11 +17,11 @@ public class TestMain {
 		{
 		  System.out.println("debut test");
 				
-//			DesiredCapabilities cap=new DesiredCapabilities();
-//		    cap.setBrowserName("chrome");
-//		    WebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
-		    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
+			DesiredCapabilities cap=new DesiredCapabilities();
+		    cap.setBrowserName("chrome");
+		    WebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+//		    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+//			WebDriver driver = new ChromeDriver();
 			driver.manage().window().maximize();
 		    driver.get("https://www.facebook.com/");
 			driver.findElement(By.xpath("//input[@id='email']")).sendKeys("0652455762");
